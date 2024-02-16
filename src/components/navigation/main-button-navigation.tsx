c0aee2dc5b13c3195100dd2c6050a72f2e11c8fa
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-02-16 13:58:23 +0100

    style: rm github icon

--- src/components/navigation/main-button-navigation.tsx
+++ src/components/navigation/main-button-navigation.tsx
@@ -1,7 +1,5 @@
 import { Locale } from '@/src/lib/lang/i18.config';
 import { cn } from '@/src/lib/utils';
-import Link from 'next/link';
-import { Icons } from '../base/icons';
 import { buttonVariants } from '../ui/button';
 import NavigationLink from './link-navigation';
 
@@ -16,19 +14,10 @@ export default async function MainButtonNavigation({
 }: IMainButtonNav) {
   return (
     <nav className="flex flex-row gap-2 items-center">
-      <Link
-        href={'https://github.com/TobiasGleiter/nextjs-appointment-booking'}
-        target="_blank"
-      >
-        <Icons.gitHub className="w-6 h-6 hover:translate-x-1 duration-200" />
-      </Link>
       <NavigationLink
         lang={lang}
         path={'/login'}
-        className={cn(
-          buttonVariants({ variant: 'default' }),
-          'hidden sm:block'
-        )}
+        className={cn(buttonVariants({ variant: 'default' }))}
       >
         {button.bookNow}
       </NavigationLink>
