43b1f3ca70c3677b0f360ad8fa10c6a43baf7b67
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-02-17 06:39:38 +0100

    style: added dashboard header sellers page

--- src/app/[lang]/(dashboard)/dashboard/sellers/page.tsx
+++ src/app/[lang]/(dashboard)/dashboard/sellers/page.tsx
@@ -1,12 +1,27 @@
+import { DashboardHeader } from '@/src/components/navigation/header/dashboard-header';
 import { readAllSellers } from '@/src/lib/database/collection/seller/read-seller';
+import { Locale } from '@/src/lib/lang/i18.config';
+import { getDictionary } from '@/src/lib/lang/lang';
 import { columns } from './columns';
 import { DataTable } from './data-table';
 
-export default async function SellersManagementPage() {
+interface SellersManagementPageProps {
+  children?: React.ReactNode;
+  params: { lang: Locale };
+}
+
+export default async function SellersManagementPage({
+  params: { lang },
+}: SellersManagementPageProps) {
+  const { page } = await getDictionary(lang);
   const data = await readAllSellers();
 
   return (
     <div>
+      <DashboardHeader
+        heading={page.sellers.headline}
+        text={page.sellers.description}
+      />
       <DataTable columns={columns} data={data} />
     </div>
   );
